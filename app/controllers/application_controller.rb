e92afa062f9094c87601fc8c6c91436106b8c52a
Nolan Hughes <nhughes987@gmail.com>
2018-01-24 16:42:07 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,9 @@ require 'sinatra/base'
 class App < Sinatra::Base
   set :views, Proc.new { File.join(root, "../views/") }
 
-  # get '/' do
-  #   erb :'index'
-  # end
+  get '/' do
+    erb :'index'
+  end
 
   get '/' do
     erb :'new'
