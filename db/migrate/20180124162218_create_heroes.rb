31bb5e2e63d778e9bc5bdfffec29bff92f0699af
Nolan Hughes <nhughes987@gmail.com>
2018-01-24 16:24:38 +0000

    automatically backed up by learn

--- db/migrate/20180124162218_create_heroes.rb
+++ db/migrate/20180124162218_create_heroes.rb
@@ -2,8 +2,9 @@ class CreateHeroes < ActiveRecord::Migration[5.1]
   def change
     create_table :teams do |column|
       t.string :name
-      t.string :motto
+      t.string :power
+      t.string :bio
     end
   end
-  
+
 end
